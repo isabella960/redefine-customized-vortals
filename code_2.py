a23d1b4ded5d4c2aa028861380e33bce687b1c5f
Isabella <143600689+isabella960@users.noreply.github.com>
2023-10-29 07:47:35 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def main():
+    for item in data:
+if __name__ == "__main__":
+def generate_random_data():
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+
+    main()
+    return data
+import random
